f0c3b9a48ab44bdfa9559985955c11134bd05734
crisanbe289 <crisanbe289@gmail.com>
2021-12-26 14:19:06 -0500

    (DetailScreen): Se crea Navbar para devolver al inicio

--- app/src/main/java/tech/yeswecode/coffe4coders/ui/screens/DetailScreen.kt
+++ app/src/main/java/tech/yeswecode/coffe4coders/ui/screens/DetailScreen.kt
@@ -3,25 +3,40 @@ package tech.yeswecode.coffe4coders.ui.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import tech.yeswecode.coffe4coders.ui.components.CountryISO
+import tech.yeswecode.coffe4coders.ui.components.CustomAppBar
 
 @Composable
-fun DetailScreen(navController: NavController, countryISO: CountryISO){
-    Column() {
-        Text("Soy un Producto de: ${countryISO.iso}", style = MaterialTheme.typography.h3)
-        Button(onClick = {
-            navController.navigate("feed"){
-                popUpTo("feed")//para que vuelva al mismo feed donde inicio
+fun DetailScreen(navController: NavController, countryISO: CountryISO) {
+    Scaffold(
+        topBar = {
+            CustomAppBar(navigationIcon = Icons.Filled.ArrowBack) {
+                navController.navigate("feed") {
+                    popUpTo("feed")//para que vuelva al mismo feed donde inicio
+                }
             }
-        }) {
-            Text(text = "Volver")
+        },
+        content = {
+            Column() {
+                Text("Soy un Producto de: ${countryISO.iso}", style = MaterialTheme.typography.h3)
+                Button(onClick = {
+                    navController.navigate("feed") {
+                        popUpTo("feed")//para que vuelva al mismo feed donde inicio
+                    }
+                }) {
+                    Text(text = "Volver")
+                }
+            }
         }
-    }
+    )
 }
 
 
@@ -29,7 +44,7 @@ fun DetailScreen(navController: NavController, countryISO: CountryISO){
     showBackground = true
 )
 @Composable
-fun DetailScreenPreview(){
+fun DetailScreenPreview() {
     val navController = rememberNavController()
     DetailScreen(navController, CountryISO.BRA)
 }
