2050bc71e84a322c5d4e60e09b9e74a085ce1d59
crisanbe <crisanbe289@gmail.com>
2021-12-27 09:37:11 -0500

    (DetailScreen):Creando pantalla Detalle

--- app/src/main/java/tech/yeswecode/coffe4coders/ui/screens/DetailScreen.kt
+++ app/src/main/java/tech/yeswecode/coffe4coders/ui/screens/DetailScreen.kt
@@ -1,6 +1,9 @@
 package tech.yeswecode.coffe4coders.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -8,11 +11,16 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import tech.yeswecode.coffe4coders.ui.components.CountryISO
-import tech.yeswecode.coffe4coders.ui.components.CustomAppBar
+import tech.yeswecode.coffe4coders.ui.components.*
+import tech.yeswecode.coffe4coders.ui.theme.Coffe4codersTheme
 
 @Composable
 fun DetailScreen(navController: NavController, countryISO: CountryISO) {
@@ -25,14 +33,42 @@ fun DetailScreen(navController: NavController, countryISO: CountryISO) {
             }
         },
         content = {
-            Column() {
-                Text("Soy un Producto de: ${countryISO.iso}", style = MaterialTheme.typography.h3)
-                Button(onClick = {
-                    navController.navigate("feed") {
-                        popUpTo("feed")//para que vuelva al mismo feed donde inicio
+            Column(
+                modifier = Modifier.verticalScroll(rememberScrollState())
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(400.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = countryISO.getBackgroundImage()),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+                Column(modifier = Modifier.padding(16.dp)) {
+                    TitleText(title = "Café de Colombia")
+                    Text(" Lorem ipsum dolor sit amet, consectetur adipisci elit. ",
+                        style = MaterialTheme.typography.caption)
+                    Spacer(modifier = Modifier.height(24.dp))
+                    BodyText(body = " Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. ")
+                    Spacer(modifier = Modifier.height(24.dp))
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Text(" $ 35.0 USD",
+                        style = MaterialTheme.typography.h5,
+                        textAlign = TextAlign.End
+                        )
+                        CustomButton(label = "Continuar") {
+                            navController.navigate("checkout"){
+                                launchSingleTop = true
+                            }
+
+                        }
                     }
-                }) {
-                    Text(text = "Volver")
                 }
             }
         }
@@ -46,5 +82,7 @@ fun DetailScreen(navController: NavController, countryISO: CountryISO) {
 @Composable
 fun DetailScreenPreview() {
     val navController = rememberNavController()
-    DetailScreen(navController, CountryISO.BRA)
+    Coffe4codersTheme() {
+        DetailScreen(navController, CountryISO.BRA)
+    }
 }
