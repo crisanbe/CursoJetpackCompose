139ae0391c89e29b55771398e85cca9b970b2bb6
crisanbe <crisanbe289@gmail.com>
2021-12-27 16:18:01 -0500

    (TexField):Creando campo de Texto

--- app/src/main/java/tech/yeswecode/coffe4coders/ui/screens/CheckoutScreen.kt
+++ app/src/main/java/tech/yeswecode/coffe4coders/ui/screens/CheckoutScreen.kt
@@ -1,2 +1,21 @@
 package tech.yeswecode.coffe4coders.ui.screens
 
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+import tech.yeswecode.coffe4coders.ui.components.CustomTextField
+
+@Composable
+fun CheckoutScreen(){
+  var name by remember { mutableStateOf("") }
+
+    CustomTextField(value = name, placeholder = "Nombre" ){
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun CheckoutScreenPreview(){
+  CheckoutScreen()
+}
